9b907f20f834840e205995bee7385a44a81fd97e
GabrielPeixoto111 <gdpacademico@gmail.com>
2026-06-02 16:13:05 -0300

    chore: Formata o logger.ts utilizando biome

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -4,15 +4,15 @@ import { env } from './env'
 const isDev = env.NODE_ENV === 'development'
 
 export const logger = pino({
-    level: env.LOG_LEVEL,
-    ...(isDev && {
-        transport: {
-            target: 'pino-pretty',
-            options: {
-                colorize: true,
-                translateTime: 'HH:MM:ss',
-                ignore: 'pid,hostname',
-            },
-        },
-    }),
+  level: env.LOG_LEVEL,
+  ...(isDev && {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+      },
+    },
+  }),
 })
